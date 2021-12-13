f843bc417113ae69bbce251319ccc63b94769d6b
eloicasamayor <37459666+eloicasamayor@users.noreply.github.com>
2021-12-13 23:15:53 +0100

    styles applied with scss modules

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import React from "react"
 const Footer = () => {
   return (
     <footer>
-      <p>Created by Eloi Casamayor 2021</p>
+      <p>Created by Eloi Casamayor © 2021</p>
     </footer>
   )
 }
